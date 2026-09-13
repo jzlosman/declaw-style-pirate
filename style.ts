595939303c2cdcc7e90d98917d65dae9ad9f517d
Jeremy Zaborowski <jeremy@wonderly.com>
2026-09-13 01:54:00 -0400

    Prepare Pirate 0.1.1 release with Declaw 0.4 support

--- style.ts
+++ style.ts
@@ -4,7 +4,7 @@ export const piratePlugin: DeclawStylePlugin = {
   apiVersion: 1,
   id: "pirate",
   name: "Pirate styles",
-  version: "0.1.0",
+  version: "0.1.1",
   status: "active",
   styles: [{
     id: "pirate/pirate",
